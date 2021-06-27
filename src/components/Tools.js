87d22b1fa374606bbc74fc431668dcfeb6f4d657
roushansourav <roushansourav@outlook.com>
2021-06-27 13:24:40 +0530

    tooltip

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,20 +1,43 @@
 import React from "react";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import GetAppIcon from "@material-ui/icons/GetApp";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 function Tools({ handleDownload, handleRun }) {
   return (
     <div className="tooltip-container">
-      <PlayArrowIcon
-        onClick={handleRun}
-        className="tools-icon"
-        style={{ fontSize: "2rem" }}
-      />
-      <GetAppIcon
-        className="tools-icon"
-        onClick={handleDownload}
-        style={{ fontSize: "2rem" }}
-      />
+      <OverlayTrigger
+        key="top"
+        placement="top"
+        overlay={
+          <Tooltip id="tooltip-top">
+            <strong>Run</strong>
+          </Tooltip>
+        }
+      >
+        <PlayArrowIcon
+          onClick={handleRun}
+          className="tools-icon"
+          style={{ fontSize: "2rem" }}
+        />
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        key="download"
+        placement="top"
+        overlay={
+          <Tooltip id="tooltip-download">
+            <strong>Download</strong>
+          </Tooltip>
+        }
+      >
+        <GetAppIcon
+          className="tools-icon"
+          onClick={handleDownload}
+          style={{ fontSize: "2rem" }}
+        />
+      </OverlayTrigger>
     </div>
   );
 }
